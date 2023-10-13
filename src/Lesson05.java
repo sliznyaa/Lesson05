ab9d73eb64272f584c3baa37de73e554f3d7d2fe
sliznyaa <misterslaim122@gmail.com>
2023-10-13 20:18:20 +0300

    yes

---
+++ src/Lesson05.java
@@ -0,0 +1,14 @@
+public class Lesson05 {
+    public static void main (String[]args){
+        int randomNum = 5;
+
+        for (int i = 0; i < 10; i+=2){ // напишет только парные числа
+            System.out.println(i);
+            //randomNum++;
+        }
+        for (int i = 1; i < 10; i+=2){ // напишет только не парные числа
+            System.out.println(i);
+            //randomNum++;
+        }
+    }
+}
